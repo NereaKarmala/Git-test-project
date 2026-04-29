88984c0f6e409f056e379eb698a69c14e1c8bde1
NereaKarmala <nerea.karmala@gmail.com>
2026-04-29 17:00:49 +0300

    Add files via upload

---
+++ new_file.py
@@ -0,0 +1,7 @@
+print("We made some changes")
+
+print("Here is some new text")
+print("We made some changes in the previous example")
+
+print("we made some changes")
+print("again some new code here")
